751ce44f1a6e19c447d00688f7f26ff2848380a7
jyr <jyr@wavers.co.kr>
2022-07-06 00:58:44 +0900

    06-2-2-뷰 크기, 간격, 표시여부

---
+++ lab6/src/main/java/io/jyryuitpro/lab6/test3/Test4Activity.kt
@@ -0,0 +1,26 @@
+package io.jyryuitpro.lab6.test3
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import io.jyryuitpro.lab6.R
+
+class Test4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test4)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+
+        button1.setOnClickListener {
+            button2.visibility = View.VISIBLE
+        }
+
+        button3.setOnClickListener {
+            button2.visibility = View.INVISIBLE
+        }
+    }
+}
