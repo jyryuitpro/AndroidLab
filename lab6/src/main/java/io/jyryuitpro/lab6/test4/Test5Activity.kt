4b19cf4012a8332d8fc5b1cfb7bf3af2b4671dff
jyr <jyr@wavers.co.kr>
2022-07-06 01:55:12 +0900

    06-3-기본적인 뷰 살펴보기

---
+++ lab6/src/main/java/io/jyryuitpro/lab6/test4/Test5Activity.kt
@@ -0,0 +1,12 @@
+package io.jyryuitpro.lab6.test4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.jyryuitpro.lab6.R
+
+class Test5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test5)
+    }
+}
