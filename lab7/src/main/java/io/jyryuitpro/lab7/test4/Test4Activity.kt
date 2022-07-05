41fedf9cdc7eea3a1ad255865db48fb269e9e9a8
jyr <jyr@wavers.co.kr>
2022-07-06 08:53:18 +0900

    07-4-표 형태로 배치 - GridLayout

---
+++ lab7/src/main/java/io/jyryuitpro/lab7/test4/Test4Activity.kt
@@ -0,0 +1,12 @@
+package io.jyryuitpro.lab7.test4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.jyryuitpro.lab7.R
+
+class Test4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test4)
+    }
+}
