045551f531edf7f001df49a9aab83a34a859c2c8
jyr <jyr@wavers.co.kr>
2022-07-09 18:09:03 +0900

    09-2-리소스 조건 설정

---
+++ lab9/src/main/java/io/jyryuitpro/lab9/test2/Test2Activity.kt
@@ -0,0 +1,12 @@
+package io.jyryuitpro.lab9.test2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.jyryuitpro.lab9.R
+
+class Test2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test2)
+    }
+}
