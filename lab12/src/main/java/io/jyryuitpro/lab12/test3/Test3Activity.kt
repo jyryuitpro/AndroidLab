fb8ea564df51c184971bbb5439adf9b2bfa051f7
jyr <jyr@wavers.co.kr>
2022-07-12 06:37:28 +0900

    12-3,4-내비게이션뷰, 확장된 플로팅 액션 버튼

---
+++ lab12/src/main/java/io/jyryuitpro/lab12/test3/Test3Activity.kt
@@ -0,0 +1,12 @@
+package io.jyryuitpro.lab12.test3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.jyryuitpro.lab12.R
+
+class Test3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test3)
+    }
+}
