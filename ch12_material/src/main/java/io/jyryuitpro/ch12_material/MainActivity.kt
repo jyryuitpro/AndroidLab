723f3449515980e94c0406297d443ff26cc7ad64
jyr <jyr@wavers.co.kr>
2022-07-16 11:18:21 +0900

    12-5-머티리얼 라이브러리로 화면 구성하기

---
+++ ch12_material/src/main/java/io/jyryuitpro/ch12_material/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.jyryuitpro.ch12_material
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
